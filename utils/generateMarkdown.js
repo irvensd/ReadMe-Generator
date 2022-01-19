83d95184637d624d6cd3bac46d82814f113d0315
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-01-19 11:19:14 -0500

    generateMarkdown page

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,10 @@
+function generatePage(text) {
+  return `# ${text.title}
+  ${renderLicenseBadge(data.license)}
+  `
+}
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
