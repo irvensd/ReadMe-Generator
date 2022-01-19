ead8b48f2c7927a26026caf43ec4cebcf599d805
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-01-19 10:19:48 -0500

    Trying to generate README file

--- index.js
+++ index.js
@@ -12,30 +12,116 @@
 // // Function call to initialize app
 // init();
 
-const inquirer = require("inquirer");
-const fs = require("fs");
+// const fs = require('fs');
+// const generateOutput = require('./src/page-template.js')
 
-const questionInput = () => {
+// const pageHTML = generateOutput(name, github)
+
+// fs.writeFile('index.html', generateOutput(name, github), err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+
+const promptUser = () => {
+
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
   return inquirer.prompt([
     {
-      type: "input",
-      name: "name",
-      message: "What is your project title? (Required)",
-      validate: (titleInput) => {
-        if (titleInput) {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
           return true;
         } else {
-          console.log("Please enter project name");
+          console.log('Please enter your name!');
           return false;
         }
-      },
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
     },
     {
       type: "input",
-      name: "description",
-      message: "Desxcription of your project:",
+      name: "email",
+      message: "What is your email address?",
     },
+    {
+      type: 'input',
+      name: 'project',
+      message: 'What is your project title? (Required)',
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project description!');
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Installation instructions for your project: ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Usage information your project: ",
+    },
+    {
+      type: "input",
+      name: "guidelines",
+      message: "Contribution Guidelines for your project: ",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Test instructions for your project: ",
+    },
+   
   ]);
 };
 
-questionInput().then((answers) => console.log(answers));
+promptUser()
+  .then(results => {
+    console.log(results);
+    // will be uncommented in lesson 4
+    const pageHTML = generatePage();
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
+
